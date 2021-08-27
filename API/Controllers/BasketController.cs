b1670c448ebd5268474ea71b8544ce52d15af6f4
Manuchehr Ahmadkhonov <manoujon@gmail.com>
2021-08-27 09:48:59 -0400

    End of section 16

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -2,37 +2,43 @@ using System.Threading.Tasks;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Core.Entities;
+using API.Dtos;
+using AutoMapper;
 
 namespace API.Controllers
 {
   public class BasketController : BaseApiController
   {
     private readonly IBasketRepository _basketRepository;
-    public BasketController(IBasketRepository basketRepository)
+    private readonly IMapper _mapper;
+    public BasketController(IBasketRepository basketRepository, IMapper mapper)
     {
+      _mapper = mapper;
       _basketRepository = basketRepository;
     }
 
     [HttpGet]
     public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
     {
-        var basket = await _basketRepository.GetBasketAsync(id);
+      var basket = await _basketRepository.GetBasketAsync(id);
 
-        return Ok(basket ?? new CustomerBasket(id));
+      return Ok(basket ?? new CustomerBasket(id));
     }
 
     [HttpPost]
-    public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket) 
+    public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
     {
-        var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+      var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
 
-        return Ok(updatedBasket);
+      var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
+
+      return Ok(updatedBasket);
     }
 
     [HttpDelete]
-    public async Task DeleteBasketAsync(string id) 
+    public async Task DeleteBasketAsync(string id)
     {
-        await _basketRepository.DeleteBasketAsync(id);
+      await _basketRepository.DeleteBasketAsync(id);
     }
   }
 }
--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -4,24 +4,42 @@ using Microsoft.OpenApi.Models;
 
 namespace API.Extensions
 {
-    public static class SwaggerServiceExtensions
+  public static class SwaggerServiceExtensions
+  {
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
     {
-        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+
+        var securitySchema = new OpenApiSecurityScheme
         {
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+          Description = "JWT Auth Bearer Scheme",
+          Name = "Authorization",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.Http,
+          Scheme = "bearer",
+          Reference = new OpenApiReference
+          {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+          }
+        };
 
-            return services;
-        }
+        c.AddSecurityDefinition("Bearer", securitySchema);
+        var securityRequirement = new OpenApiSecurityRequirement { { securitySchema, new[] { "Bearer" } } };
+        c.AddSecurityRequirement(securityRequirement);
+      });
 
-        public static IApplicationBuilder UseSwaggerDocumention(this IApplicationBuilder app)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-
-            return app;
-        }
+      return services;
     }
+
+    public static IApplicationBuilder UseSwaggerDocumention(this IApplicationBuilder app)
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+
+      return app;
+    }
+  }
 }
