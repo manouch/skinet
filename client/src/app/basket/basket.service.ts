1d4c46270f97e0ec4b559ed05f7139b758435bfe
Manuchehr Ahmadkhonov <manoujon@gmail.com>
2021-11-06 13:26:27 -0400

    End of section 21

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -20,9 +20,24 @@ export class BasketService {
 
   constructor(private http: HttpClient) { }
 
+  createPaymentIntent() {
+    return this.http.post(this.baseUrl + 'payments/' + this.getCurrentBasketValue().id, {})
+      .pipe(
+        map((basket: IBasket) => {
+          this.basketSource.next(basket);
+          // console.log(this.getCurrentBasketValue());
+
+        })
+      )
+  }
+
   setShippingPrice(deliveryMethod: IDeliveryMethod) {
     this.shipping = deliveryMethod.price;
+    const basket = this.getCurrentBasketValue();
+    basket.deliveryMethodId = deliveryMethod.id;
+    basket.shippingPrice = deliveryMethod.price;
     this.calculateTotals();
+    this.setBasket(basket);
   }
 
   getBasket(id: string) {
@@ -31,6 +46,7 @@ export class BasketService {
         map((basket: IBasket) => {
           this.basketSource.next(basket);
           // console.log(this.getCurrentBasketValue());
+          this.shipping = basket.shippingPrice;
           this.calculateTotals();
         })
       );
